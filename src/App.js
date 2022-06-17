0058f3bb9f1225482bfc25e64778253234c98489
marcos.asdes <marco.engc@poli.ufrj.br>
2022-06-17 08:41:53 -0300

    fix signin route and install react-loader-spinner

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="" element={<LoginPage />} />
+        <Route path="/" element={<LoginPage />} />
         <Route path="/sign-up" element={<RegisterPage />} />
         <Route path="/timeline" element={<HomePage />} />
       </Routes>
