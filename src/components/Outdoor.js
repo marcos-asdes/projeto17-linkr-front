be9f022abbbf054fb8dcd2d15e8b0b0add717074
marcos.asdes <marco.engc@poli.ufrj.br>
2022-06-17 05:12:24 -0300

    signin component and LoginPage working

--- src/components/Outdoor.js
+++ src/components/Outdoor.js
@@ -1,4 +1,4 @@
-import { LeftSide } from "../pages/LoginPage/style.js";
+import styled from "styled-components";
 
 export default function Outdoor() {
     return (
@@ -13,3 +13,43 @@ export default function Outdoor() {
         </LeftSide>
     )
 }
+
+const LeftSide = styled.section`
+    & {
+        display: flex;
+        height: 100vh;
+        width: calc(100% - 40vw);
+        left: 0;
+        top: 0;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        text-align: left;
+        background-color: #151515;
+        box-shadow: 4px 0px 4px rgba(0, 0, 0, 0.25);
+    }
+
+    h1 {
+        width: 233px;
+        height: 117px;
+        font-family: 'Passion One';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 106px;
+        line-height: 117px;
+        letter-spacing: 0.05em;
+        color: #FFFFFF;
+    }
+
+    h2 {
+        width: 442px;
+        height: 60px;
+        font-family: 'Oswald';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 43px;
+        line-height: 43px;
+        color: #FFFFFF;
+    }
+`
--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -1,20 +1,13 @@
-import { useState } from "react";
-
 import { Content } from "./style";
 
 import SignIn from "../../components/SignIn.js";
 import Outdoor from "../../components/Outdoor.js";
 
 export default function LoginPage() {
-    const [loading, setLoading] = useState(false);
-
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-
     return (
         <Content>
             <Outdoor></Outdoor>
-            <SignIn email={email} password={password} setEmail={setEmail} setPassword={setPassword} loading={loading} setLoading={setLoading} ></SignIn>
+            <SignIn></SignIn>
         </Content>
     )
 }
